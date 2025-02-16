7315da334cfcff78897715f6fc61dac8c8ee8f7a
Omar-khaled2 <kmoroo3545@gmail.com>
2025-02-16 23:47:43 +0200

    clinic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
-import LandingPage from './components/layout/landingPage'
 import './App.css';
 import TopBar from './components/TopBar';
 import Footer from './components/layout/Footer';
 import { motion } from 'framer-motion';
 import { Route, Routes } from 'react-router-dom';
 import Appoiment from './components/layout/Appoiment';
+import LandingPage from './components/LandingPage';
 
 export default class App extends Component {
   render() {
@@ -14,7 +14,7 @@ export default class App extends Component {
         <TopBar/>
         <Navbar />
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/' element={<LandingPage/>} />
         <Route path='/appointment' element={<Appoiment />} />
       </Routes>
 
